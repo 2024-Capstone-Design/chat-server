fa14afa88f3b4a9124c0b11c09bb37bb075c22dc
gaguriee <74501631+gaguriee@users.noreply.github.com>
2024-05-18 22:19:31 +0900

    feat : health-check endpoint

---
+++ src/main/java/com/seoultech/capstone/domain/auth/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.seoultech.capstone.domain.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @GetMapping("/health-check")
+    public ResponseEntity<Void> checkHealthStatus() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
